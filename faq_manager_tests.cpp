9024719e47b558cb858485531a4da77312e0d218
Volodymyr Salo <volodymyr.salo@gmail.com>
2024-05-06 23:05:06 +0200

    Add more tests and refactor existing ones to adhere to the AAA principle
    Signed-off-by: Volodymyr Salo <volodymyr.salo@gmail.com>

--- faq_manager_tests.cpp
+++ faq_manager_tests.cpp
@@ -1,31 +1,94 @@
 #include <gtest/gtest.h>
-
 #include "faqmanager.h"
 
-// Test adding a question and retrieving answers
-TEST(FAQManagerTest, AddAndGetAnswers) {
+class FAQManagerTest : public ::testing::Test {
+protected:
     FAQManager faqManager;
-    std::vector<std::string> answers = {"Pizza", "Spaghetti", "Ice cream"};
-    std::string question = "What is Peter's favorite food?";
 
+    void SetUp() override {
+        // This setup runs before each test if needed.
+    }
+};
+
+TEST_F(FAQManagerTest, AddAndGetSingleAnswer) {
+    // Arrange
+    std::string question = "What is Peter's favorite food?";
+    std::vector<std::string> answers = {"Pizza"};
+
+    // Act
     faqManager.addQuestion(question, answers);
     std::vector<std::string> retrievedAnswers = faqManager.getAnswers(question);
 
+    // Assert
     EXPECT_EQ(answers, retrievedAnswers);
 }
 
-// Test querying a question that does not exist
-TEST(FAQManagerTest, QueryNonexistentQuestion) {
-    FAQManager faqManager;
-    std::string question = "When is Peter's birthday?";
+TEST_F(FAQManagerTest, AddAndGetMultipleAnswers) {
+    // Arrange
+    std::string question = "What is Peter's favorite food?";
+    std::vector<std::string> answers = {"Pizza", "Spaghetti", "Ice cream"};
 
-    std::vector<std::string> expected = {"the answer to life, universe and everything is 42"};
-    std::vector<std::string> answer = faqManager.getAnswers(question);
+    // Act
+    faqManager.addQuestion(question, answers);
+    std::vector<std::string> retrievedAnswers = faqManager.getAnswers(question);
 
-    EXPECT_EQ(expected, answer);
+    // Assert
+    EXPECT_EQ(answers, retrievedAnswers);
 }
 
-// You can add more tests to cover all functionalities
+TEST_F(FAQManagerTest, NonExistentQuestion) {
+    // Arrange
+    std::string question = "When is Peter's birthday?";
+
+    // Act
+    std::vector<std::string> retrievedAnswers = faqManager.getAnswers(question);
+
+    // Assert
+    std::vector<std::string> expected = {"the answer to life, universe and everything is 42"};
+    EXPECT_EQ(expected, retrievedAnswers);
+}
+
+TEST_F(FAQManagerTest, OverwriteQuestion) {
+    // Arrange
+    std::string question = "What is the best programming language?";
+    std::vector<std::string> firstAnswers = {"C++"};
+    std::vector<std::string> secondAnswers = {"Python", "JavaScript"};
+
+    // Act
+    faqManager.addQuestion(question, firstAnswers);
+    faqManager.addQuestion(question, secondAnswers);
+    std::vector<std::string> retrievedAnswers = faqManager.getAnswers(question);
+
+    // Assert
+    EXPECT_EQ(secondAnswers, retrievedAnswers);
+}
+
+TEST_F(FAQManagerTest, MaxLengthInput) {
+    // Arrange
+    std::string question(255, 'a'); // 255 'a' characters
+    std::string answer(255, 'b'); // 255 'b' characters
+    std::vector<std::string> answers = {answer};
+
+    // Act
+    faqManager.addQuestion(question, answers);
+    std::vector<std::string> retrievedAnswers = faqManager.getAnswers(question);
+
+    // Assert
+    EXPECT_EQ(answers, retrievedAnswers);
+}
+
+TEST_F(FAQManagerTest, SpecialCharacters) {
+    // Arrange
+    std::string question = "Is it correct to use ? and * in names?";
+    std::vector<std::string> answers = {"Yes, in some cases.", "No, it's generally bad practice."};
+
+    // Act
+    faqManager.addQuestion(question, answers);
+    std::vector<std::string> retrievedAnswers = faqManager.getAnswers(question);
+
+    // Assert
+    EXPECT_EQ(answers, retrievedAnswers);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
